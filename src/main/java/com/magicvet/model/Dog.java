071f0f7ed830862538b5cfce39becabf66b31f8f
Kateryna Miroshnychenko <kateryna.miroshnychenko@gmail.com>
2024-04-25 08:46:45 +0300

    REQ-04: add toString(), equals(), hashCode() methods to Pet and Dog classes

--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -1,7 +1,34 @@
 package main.java.com.magicvet.model;
 
+import java.util.Objects;
+
 public class Dog extends Pet{
     private String size;
+
+    public String toString(){
+        return "Dog {"
+               // + "type = " + this.getType() + ", "
+                + "name = " + this.getName()
+                + ", sex = " + this.getSex()
+                + ", age = " + this.getAge()
+                + ", size = " + size
+                + ", ownerName = " + this.getOwnerName()
+                + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(size, dog.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
 
     public String getSize() {
         return size;
