c6c734e0cdd311c4c1f1d812111ee09d0c6b5967
Kateryna Miroshnychenko <kateryna.miroshnychenko@gmail.com>
2024-05-03 04:41:21 +0300

    REQ-06:add sort dogs by age and by name

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,5 +1,7 @@
 package main.java.com.magicvet;
 
+import main.java.com.magicvet.comparator.DogAgeComparator;
+import main.java.com.magicvet.comparator.DogNameComparator;
 import main.java.com.magicvet.comparator.DogSizeComparator;
 import main.java.com.magicvet.model.Dog;
 
@@ -17,9 +19,38 @@ public class Sandbox {
         };
 
         Arrays.sort(dogs, new DogSizeComparator());
-
+        System.out.println("\n\nSorted by size:");
         for (Dog dog : dogs){
             System.out.println(dog.getSize());
+        }
+
+        Dog[] dogs2 = {
+                new Dog("Olly", "male" , "5 years",Dog.L),
+                new Dog("Ben", "male" , "2 years 4 months",Dog.XS),
+                new Dog("Rex", "male" , "11 years 6 months",Dog.M),
+                new Dog("Mimi", "female" , "6 years 1 month",Dog.S),
+                new Dog("Bobby", "male" , "2 months",Dog.L),
+                new Dog("Linda", "female" , "3 years",Dog.XS),
+                new Dog("Alf", "male" , "4 years 7 months",Dog.XL),
+                new Dog("Astra", "female" , "11 months",Dog.M),
+        };
+
+        Arrays.sort(dogs2, new DogSizeComparator());
+        System.out.println("\n\nSorted by size:");
+        for (Dog dog : dogs2){
+            System.out.println(dog.getSize());
+        }
+
+        Arrays.sort(dogs2, new DogAgeComparator());
+        System.out.println("\n\nSorted by age:");
+        for (Dog dog : dogs2){
+            System.out.println(dog.getAge());
+        }
+
+        Arrays.sort(dogs2, new DogNameComparator());
+        System.out.println("\n\nSorted by name:");
+        for (Dog dog : dogs2){
+            System.out.println(dog.getName());
         }
     }
 }
--- src/main/java/com/magicvet/service/PetService.java
+++ src/main/java/com/magicvet/service/PetService.java
@@ -5,9 +5,19 @@ import main.java.com.magicvet.model.Cat;
 import main.java.com.magicvet.model.Dog;
 import main.java.com.magicvet.model.Pet;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PetService {
     private static final String DOG_TYPE = "dog";
     private static final String CAT_TYPE = "cat";
+    private static final String AGE_PATTERN = "^(\\d+\\s*(y|year|years)$|" +
+                                                "\\d+\\s*(m|month|months)$|" +
+                                                "\\d+\\s*(y|year|years)\\s*(and)*\\s*\\d+\\s*(m|month|months)$)";
+
+    private static final String SEX_PATTERN = "^(male|female)$";
+    private static final String SIZE_PATTERN = "^(XS|S|M|L|XL)$";
+
     public Pet registerNewPet() {
         Pet pet = null;
         System.out.print("Type (dog / cat): ");
@@ -26,21 +36,33 @@ public class PetService {
         Pet pet = type.equals(CAT_TYPE) ? new Cat() : new Dog();
         pet.setType(type);
 
-        System.out.print("Age: ");
-        pet.setAge(Main.SCANNER.nextLine());
+        System.out.print("Age: (For ex: 5 years / 3 months/ 2 years and 1 month) ");
+        pet.setAge(validateInputForPattern(Main.SCANNER.nextLine(),AGE_PATTERN," 5 years / 3 months/ 2 years and 1 month"));
 
         System.out.print("Name: ");
-        pet.setName(Main.SCANNER.nextLine());
+        pet.setName(Main.SCANNER.nextLine().trim());
 
         System.out.print("Sex (male / female): ");
-        pet.setSex(Main.SCANNER.nextLine());
+        pet.setSex(validateInputForPattern(Main.SCANNER.nextLine(),SEX_PATTERN,"male or female"));
 
         if (type.equals(DOG_TYPE)){
             System.out.print("Size (XS / S / M / L / XL): ");
-            ((Dog) pet).setSize(Main.SCANNER.nextLine().toUpperCase());
+            ((Dog) pet).setSize(validateInputForPattern(Main.SCANNER.nextLine(),SIZE_PATTERN,"XS / S / M / L / XL"));
         }
 
         return pet;
+    }
+
+    private String validateInputForPattern(String input, String patternString, String validInput) {
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(input.trim());
+        while (!matcher.matches()){
+            System.out.println("The input '" + input + "' is not valid! Valid input is: " + validInput);
+            System.out.print("Please try again: ");
+            input = Main.SCANNER.nextLine();
+            matcher = pattern.matcher(input.trim());
+        }
+        return input.trim();
     }
 
 }
